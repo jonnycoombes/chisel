9437161d945d697f597ad5a71e53f131eb680729
Jonny Coombes <jcoombes@jcs-software.co.uk>
2023-10-25 17:08:51 +0100

    doc: finished off basic overview of scanning

--- chisel-lexers/src/lib.rs
+++ chisel-lexers/src/lib.rs
@@ -1,19 +1,24 @@
 //! # Overview
-//! This crate contains the lexical analysis backends used throughout chisel. The basic architecture
-//! is very simple, the only thing to note being that the scanning is handled separately to the
-//! lexing (production of tokens) by a common scanner implementation.
+//! This crate contains the lexical analysis backends used by *chisel*. The basic architecture
+//! is very simple - multiple lexers using a common scanning implementation.
 //!
-//! Each lexer consumes from a *scanner* instance, and attempts to construct *tokens* which may be
+//! A *scanner* consumes characters from an underlying source of characters, and keeps track of the
+//! position where the character was read. It also provides some basic buffering and lookahead/pushback
+//! functionality.
+//!
+//! It's always assumed that input is read linearly and can only be read once, from start to finish.
+//!
+//! A *lexer* consumes from a scanner, and attempts to construct *tokens* which may be
 //! consumed by *parsers* further up the stack.
 //!
-//! Each lexer defines, and is capable of producing, its own set of distinct tokens
+//! A lexer defines and is capable of producing its own set of distinct tokens
 //! specific to the parsing task in hand.  (For example, the JSON lexer produces JSON-specific
 //! tokens only).
 //!
 //! ## Scanning the input
 //! The scanner operates through maintaining an internal state:
 //!
-//! - An transient buffer of characters, paired with their coordinates in the input
+//! - A transient buffer of characters, paired with their coordinates in the input
 //! - A transient pushback buffer, which can be used to temporarily push characters back into the input
 //! - A single lookahead character which can be used for LA(1) operations by a lexer  
 //! - A current position in the input
@@ -22,9 +27,9 @@
 //! At any point, a lexer can take a look at the front and the back of the buffer in order to decide
 //! whether to either *advance*, *pushback* or *lookahead*:
 //!
-//! - *advance* add another character to the scanning buffer
-//! - *pushback* take the last read character from the buffer and add to the pushback buffer
-//! - *lookahead* read one character from the input, but don't add it to the buffer (yet)
+//! - *advance*  - add another character to the scanning buffer
+//! - *pushback* - take the last read character from the buffer and add to the pushback buffer
+//! - *lookahead* - read one character from the input, but don't add it to the buffer (yet)
 //!
 //! If a fault occurs during any of these operations, the scanner returns a `Err` containing
 //! information about the nature of the fault and the location in the input where it occurred.
