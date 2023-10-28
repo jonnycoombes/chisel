e5fbc60e02b27669e84737b26c64028707e20017
Jonny Coombes <jcoombes@jcs-software.co.uk>
2023-10-28 04:34:10 +0100

    feat: addition of convenience macro

--- chisel-common/src/macros/files.rs
+++ chisel-common/src/macros/files.rs
@@ -34,3 +34,13 @@ macro_rules! lines_from_relative_file {
         BufReader::new(f).lines()
     }};
 }
+
+/// Create a reader based on a relative file
+#[macro_export]
+macro_rules! reader_from_relative_file {
+    ($f : expr) => {{
+        let path = env::current_dir().unwrap().join($f);
+        let f = File::open(path).unwrap();
+        BufReader::new(f)
+    }};
+}
